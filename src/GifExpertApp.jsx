f9e8dffda6118513a8c98d2e1f4e4d9553d36d3f
Diegocarque12 <diegocarque@hotmail.com>
2024-04-05 20:01:52 -0600

    add category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+
 export const GifExportApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory onAddCategory={onAddCategory} />
+
+      <ol>
+        {categories.map((category) => (
+          <div className="" key={category}>
+            <h3>{category}</h3>
+            <li>{category}</li>
+          </div>
+        ))}
+      </ol>
     </>
   );
 };
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function AddCategory({ onAddCategory }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+
+    setInputValue("");
+    onAddCategory(inputValue.trim());
+  };
+
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={inputValue}
+        onChange={onInputChange}
+        placeholder="Buscar Gifs"
+      />
+      {/* <button type="submit">Agregar</button> */}
+    </form>
+  );
+}
